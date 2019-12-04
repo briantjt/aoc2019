a837129b8deb830c59040f7130c0ddd1641c7074
Brian <tjtbrian@gmail.com>
2019-12-04 15:16:21 +0800

    Completed day 4

--- src/Day2/Day2.hs
+++ src/Day2/Day2.hs
@@ -16,13 +16,15 @@ day2Part1 = do
   processOpCode array 0
   readArray array 0
 
+day2Part2 :: IO (Int, Int)
 day2Part2 = do
   root  <- getCurrentDirectory
   input <- readFile (root ++ "/src/Day2/input.txt")
   let values = map (\x -> read x :: Int) $ splitOn "," input
   values <- mapM
     (\(a, b) -> do
-      array  <- newListArray (0, length values - 1) values :: IO (IOArray Int Int)
+      array <-
+        newListArray (0, length values - 1) values :: IO (IOArray Int Int)
       writeArray array 1 a
       writeArray array 2 b
       processOpCode array 0
@@ -30,7 +32,6 @@ day2Part2 = do
       if ans == 19690720 then return (a, b) else return (-1, -1)
     )
     [ (a, b) | a <- [0 .. 99], b <- [0 .. 99] ]
-
   return $ head $ filter (\(a, b) -> a > -1) values
 
 processOpCode :: IOArray Int Int -> Int -> IO (IOArray Int Int)
---
+++ src/Day4/Day4.hs
@@ -0,0 +1,32 @@
+module Day4.Day4 where
+
+import qualified Data.Map                      as M
+numbers :: [Int]
+numbers = [197487 .. 673251]
+
+splitDigits :: Int -> [Int]
+
+splitDigits i | i < 10    = [i]
+              | otherwise = splitDigits (i `quot` 10) ++ [i `mod` 10]
+
+twoSameDigits :: [Int] -> Bool
+twoSameDigits []           = False
+twoSameDigits [x         ] = False
+twoSameDigits (x : y : zs) = (x == y) || twoSameDigits (y : zs)
+
+increasingOrder :: [Int] -> Bool
+increasingOrder []           = True
+increasingOrder [a         ] = True
+increasingOrder (x : y : zs) = (x <= y) && increasingOrder (y : zs)
+
+part1passwords :: [[Int]]
+part1passwords =
+  filter (\x -> twoSameDigits x && increasingOrder x) $ map splitDigits numbers
+
+day4part1 = length part1passwords
+
+-- Count the number of digits in each password and check if there is a digit
+-- with a count of exactly 2
+day4part2 = length $ filter (any (\(a, b) -> b == 2)) $ map
+  (M.toList . M.fromListWith (+) . (\x -> zip x (repeat (1 :: Int))))
+  part1passwords
